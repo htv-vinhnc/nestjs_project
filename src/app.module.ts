5b92da3651e8ff5741ddd079b6b654f170ea85c0
Nguyễn Công Vinh <vinh592001@gmail.com>
2022-09-15 23:10:23 +0700

    setup project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [StudentModule],
+  imports: [
+    TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: '127.0.0.1',
+    port: 3306,
+    username: 'root',
+    password: 'Root8888',
+    database: 'nestjs_project',
+    entities: [],
+}),
+StudentModule
+],
   controllers: [AppController],
   providers: [AppService],
 })
